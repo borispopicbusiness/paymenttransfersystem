d6270a4420acdc1dd7be5605f0460dadd55e4975
Boris Popic <borispopic.business@gmail.com>
2025-06-16 15:47:23 +0200

    fix modifier mistake in JwtUtil and typos in application.properties

--- src/main/java/org/borispopic/paymenttransfersystem/security/JwtUtils.java
+++ src/main/java/org/borispopic/paymenttransfersystem/security/JwtUtils.java
@@ -32,7 +32,7 @@ public class JwtUtils {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
